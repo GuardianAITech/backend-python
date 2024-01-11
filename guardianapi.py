e21771c48f3812d91a8a5baa202a995c80ef7fce
HydraCoderX <hydracoder@proton.me>
2024-01-11 16:27:26 +0100

    added filters and more
    - added total balance to json (native + tokens)
    - added total at risk to json
    
    --- These are quick read data for frontend --
    - added latest 30 transactions to json
    - added filtered data
    -- total failed transactions (within these 30)
    -- total successful transactions (within these 30)
    -- total gas value (of these 30)
    -- total gas spent (of these 30 in eth)
    -- total value transferred (of these 30)

--- guardianapi.py
+++ guardianapi.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, jsonify, abort
 from dotenv import load_dotenv
-from helpers.covalent_helpers import get_approvals, get_token_balances, get_summary_transactions, extract_token_info, extract_approvals_items
+from helpers.covalent_helpers import get_approvals, get_token_balances, get_summary_transactions, extract_token_info, extract_approvals_items,get_latest_transactions,extract_transaction_info
 import os
+import asyncio
 
 load_dotenv()
 app = Flask(__name__)
@@ -11,7 +12,7 @@ secret_key = os.getenv("SECRET_KEY")
 chain = "eth-mainnet"
 
 @app.route('/scan', methods=['GET'])
-def scan():
+async def scan():
     client_key = request.headers.get('Authorization')
     if client_key != secret_key:
         abort(404)
@@ -24,14 +25,31 @@ def scan():
     token_balances = get_token_balances(covalent_api_key, chain, wallet)
     simplified_token_balances = [extract_token_info(token) for token in token_balances.get("items", [])]
     summary_transactions = get_summary_transactions(covalent_api_key, chain, wallet)
+    latest_transactions = await get_latest_transactions(covalent_api_key, chain, wallet)
+
+    extracted_transaction_info = extract_transaction_info(latest_transactions)
+
+    total_balance = sum(
+        float(token.get("pretty_quote", "0").replace('$', '').replace(',', '')) 
+        if token and token.get("pretty_quote") is not None 
+        else 0 
+        for token in token_balances.get("items", [])
+    )
+
+    total_balance_dict = {"total_balance": total_balance}
+    
+    simplified_token_balances.append(total_balance_dict)
 
     response = {
         "approvals": approvals_items,
         "token_balances": simplified_token_balances,
-        "total_transactions":summary_transactions.get("items", [])[0].get("total_count")
+        "total_transactions": summary_transactions.get("items", [])[0].get("total_count"),
+        "latest_transactions": [{"total_transaction_info": extracted_transaction_info}] + latest_transactions
     }
 
     return jsonify(response)
+
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
--- helpers/covalent_helpers.py
+++ helpers/covalent_helpers.py
@@ -1,5 +1,6 @@
 from covalent import CovalentClient
 from helpers.data_converter import convert_to_dict
+import asyncio
 
 def get_approvals(covalent_api_key, chain, wallet):
     c = CovalentClient(covalent_api_key)
@@ -16,6 +17,22 @@ def get_summary_transactions(covalent_api_key, chain, wallet):
     response = c.transaction_service.get_transaction_summary(chain, wallet)
     return convert_to_dict(response.data) if response and not response.error else {"error": "Unable to process the request"}
 
+async def get_latest_transactions(covalent_api_key, chain, wallet, limit=20):
+    c = CovalentClient(covalent_api_key)
+    try:
+            transactions = []
+            i = 0
+            async for res in c.transaction_service.get_all_transactions_for_address(chain, wallet, quote_currency="USD",no_logs=True,with_safe=True):
+                response_data = convert_to_dict(res)
+                transactions.append(response_data)
+                i = i+1
+                if i == 30:
+                    break
+            return transactions
+    except Exception as e:
+            print(e)
+
+
 def extract_token_info(token):
     return {
         "balance": token.get("balance", 0),
@@ -25,9 +42,35 @@ def extract_token_info(token):
         "contract_ticker_symbol": token.get("contract_ticker_symbol", ""),
         "logo_url": token.get("logo_url", ""),
         "native_token": token.get("native_token", False),
-        "pretty_quote": token.get("pretty_quote", ""),
+        "pretty_quote": float(token.get("pretty_quote", "0").replace('$', '').replace(',', '')) if token.get("pretty_quote") is not None else 0,
         "quote_rate": token.get("quote_rate", 0.0),
     }
 
+def extract_transaction_info(transactions):
+    total_gas_spent = sum(t.get("gas_spent", 0) for t in transactions)
+    total_gas_quote = sum(float(t.get("gas_quote", 0)) for t in transactions)
+    total_successful = sum(1 for t in transactions if t.get("successful", False))
+    total_failed = sum(1 for t in transactions if not t.get("successful", False))
+    total_value = sum(float(t.get("value_quote", 0)) for t in transactions)
+
+    totals_dict = {
+        "total_gas_spent": total_gas_spent,
+        "total_gas_quote": total_gas_quote,
+        "total_successful": total_successful,
+        "total_failed": total_failed,
+        "total_value": total_value,
+    }
+
+    return totals_dict
+
+
+
+
 def extract_approvals_items(approvals):
-    return approvals.get("items", [])
+    items = approvals.get("items", [])
+    total_at_risk = sum(float(approval.get("pretty_value_at_risk_quote", "0").replace('$', '').replace(',', '')) if approval.get("pretty_value_at_risk_quote") is not None else 0 for approval in items)
+    return {
+        "items": items,
+        "total_at_risk": total_at_risk
+    }
+
